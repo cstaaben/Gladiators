915ed2f071070d19463ac044605c44287e799beb
Corbin Staaben <cstaaben@gmail.com>
2023-05-21 21:30:59 -0700

    Implement player health and health bar UI

--- scenes/component/health_component.gd
+++ scenes/component/health_component.gd
@@ -3,6 +3,7 @@ class_name HealthComponent
 
 
 signal died
+signal health_changed
 
 @export var max_health: float = 10
 
@@ -14,7 +15,16 @@ func _ready():
 
 func damage(amount: float):
 	current = max(current - amount, 0)
+	health_changed.emit()
 	Callable(check_death).call_deferred()
+
+
+func get_health_percent():
+	if max_health <= 0:
+		return 0
+		
+	return min(current / max_health, 1)
+
 
 func check_death():
 	if current > 0:
--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -4,8 +4,18 @@ extends CharacterBody2D
 const MAX_SPEED = 125
 const ACCELERATION_SMOOTHING = 25
 
+@onready var damage_timer: Timer = $DamageTimer
+@onready var health: HealthComponent = $HealthComponent
+@onready var health_bar: ProgressBar = $HealthBar
+
+var number_colliding_bodies: int
+
 func _ready():
-	pass
+	$CollisionArea.body_entered.connect(_on_body_entered)
+	$CollisionArea.body_exited.connect(_on_body_exited)
+	damage_timer.timeout.connect(_on_damage_timer_timeout)
+	health.health_changed.connect(_on_health_changed)
+	update_health_display()
 	
 
 func _process(delta):
@@ -22,4 +32,33 @@ func get_movement_vector():
 	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 
 	return Vector2(x_movement, y_movement)
-				
+
+
+func check_deal_damage():
+	if number_colliding_bodies == 0 || !damage_timer.is_stopped():
+		return
+		
+	health.damage(1)
+	damage_timer.start()
+	print(health.current)
+	
+	
+func update_health_display():
+	health_bar.value = health.get_health_percent()
+
+
+func _on_body_entered(_body: Node2D):
+	number_colliding_bodies += 1
+	check_deal_damage()
+
+
+func _on_body_exited(_body: Node2D):
+	number_colliding_bodies -= 1
+	
+	
+func _on_damage_timer_timeout():
+	check_deal_damage()
+	
+	
+func _on_health_changed():
+	update_health_display()
