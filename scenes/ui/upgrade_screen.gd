df18b6f78dd47536a833f05993c9e39e50ed8228
Corbin Staaben <cstaaben@kochava.com>
2023-05-27 03:02:03 -0700

    UI theme and animation improvements

--- scenes/ui/upgrade_screen.gd
+++ scenes/ui/upgrade_screen.gd
@@ -9,22 +9,24 @@ signal upgrade_selected(upgrade: AbilityUpgrade)
 
 
 func _ready():
-	toggle_pause()
+	get_tree().paused = true
+	$AnimationPlayer.play("fade_in")
+	await $AnimationPlayer.animation_finished
 
 
 func set_ability_upgrades(upgrades: Array[AbilityUpgrade]):
-	for upgrade in upgrades:
+	for i in upgrades.size():
+		var upgrade = upgrades[i]
 		var card_instance = upgrade_card_scene.instantiate() as AbilityUpgradeCard
 		card_container.add_child(card_instance)
 		card_instance.set_ability_upgrade(upgrade)
+		card_instance.play_in(i * 0.15, i % 2 == 0)
 		card_instance.selected.connect(_on_upgrade_selected.bind(upgrade))
 		
 		
 func _on_upgrade_selected(upgrade: AbilityUpgrade):
 	upgrade_selected.emit(upgrade)
-	toggle_pause()
+	$AnimationPlayer.play("fade_out")
+	await $AnimationPlayer.animation_finished
+	get_tree().paused = false
 	queue_free()
-
-
-func toggle_pause():
-	get_tree().paused = !get_tree().paused
