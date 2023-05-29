b6c515b8985b4b585c4b9ffb8869add5e5943429
Corbin Staaben <cstaaben@kochava.com>
2023-05-29 00:56:19 -0700

    Fade in and out end screen

--- scenes/ui/end_screen.gd
+++ scenes/ui/end_screen.gd
@@ -3,6 +3,7 @@ extends CanvasLayer
 
 func _ready():
 	get_tree().paused = true
+	$AnimationPlayer.play("fade_in")
 
 	%RestartButton.pressed.connect(_on_restart_button_pressed)
 	%QuitButton.pressed.connect(_on_quit_button_pressed)
@@ -14,6 +15,8 @@ func set_defeat():
 	
 	
 func _on_restart_button_pressed():
+	$AnimationPlayer.play("fade_out")
+	await $AnimationPlayer.animation_finished
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/main/root.tscn")
 
