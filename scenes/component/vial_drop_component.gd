afb7bf3cdd52b58e47878707c46d183f8eb6aebf
Corbin Staaben <cstaaben@gmail.com>
2024-01-18 22:02:48 -0800

    feat: refactor vial drop component
    Refactor the vial drop component to use a weighted table of string IDs
    to randomly drop an assortment of objects. The first new drop is a
    health potion that drops at a ratio of 1:50 compared to XP vials and
    heals 2 HP.

--- scenes/component/vial_drop_component.gd
+++ scenes/component/vial_drop_component.gd
@@ -2,26 +2,49 @@ extends Node
 
 
 @export_range(0,1) var drop_percent: float = 0.5
-@export var health_component: Node
-@export var vial_scene: PackedScene
+@export var health_component: HealthComponent
+@export var xp_vial_scene: PackedScene
+@export var health_potion_scene: PackedScene
+
+var drop_table: WeightedTable = WeightedTable.new()
+var adjusted_xp_drop_percent: float = drop_percent
 
 func _ready():
-	(health_component as HealthComponent).died.connect(_on_died)
-
-func _on_died():
-	var adjusted_percent = drop_percent
+	health_component.died.connect(_on_died)
+	
 	var upgrade_count = MetaProgression.get_upgrade_quantity("experience_gain")
 	if upgrade_count > 0:
-		adjusted_percent += (0.1 * upgrade_count)
+		adjusted_xp_drop_percent += (0.1 * upgrade_count)
 		
-	if vial_scene == null or not owner is Node2D or randf() > adjusted_percent:
+	drop_table.add_item("xp", 50)
+	drop_table.add_item("health", 1)
+
+
+func _on_died():
+	if xp_vial_scene == null or health_potion_scene == null or \
+			not owner is Node2D:
+		return
+		
+	var adjusted_percent: float = drop_percent
+	var drop_obj_scene: PackedScene
+	var drop_id = drop_table.random_item() as String
+	match drop_id:
+		"xp":
+			adjusted_percent = adjusted_xp_drop_percent
+			drop_obj_scene = xp_vial_scene
+		"health":
+			drop_obj_scene = health_potion_scene
+		_:
+			push_error("unrecognized drop object id: %s" % drop_id)
+			
+	if randf() > adjusted_percent:
 		return
 
 	var spawn_position = (owner as Node2D).global_position
-	var vial_instance = vial_scene.instantiate() as Node2D
+	var drop_object_instance = drop_obj_scene.instantiate() as Node2D
 	var entities_layer = get_tree().get_first_node_in_group("entities_layer")
 	if entities_layer == null:
 		return
 		
-	entities_layer.add_child(vial_instance)
-	vial_instance.global_position = spawn_position
+	entities_layer.add_child(drop_object_instance)
+	drop_object_instance.global_position = spawn_position
--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+class_name Player
 
 
 @onready var velocity_component: VelocityComponent = $VelocityComponent
