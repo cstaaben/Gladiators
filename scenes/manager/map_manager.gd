be70dada6b5e9c4349011bf1b1649807b1a185b7
Corbin Staaben <cstaaben@kochava.com>
2024-02-01 08:24:33 -0800

    chore: add map gen debug scene

--- scenes/manager/map_manager.gd
+++ scenes/manager/map_manager.gd
@@ -1,9 +1,126 @@
 extends Node
+class_name MapManager
 
 
 @export var tile_map: TileMap
 
-var noise: FastNoiseLite
+enum Tile { GROUND }
 
-func _init():
-	noise = FastNoiseLite.new()
+var _map_size = Vector2(5, 5)
+var _walker_count: int = 10
+var _walker_loops: int = 100
+
+var _map: Array
+var _walkers: Array
+
+func _ready():
+	_map = []
+	_walkers = []
+	for i in _walker_count:
+		_walkers.push_back(Walker.new())
+		
+	for x in _map_size.x:
+		for y in _map_size.y:
+			tile_map.set_cell(0, Vector2(x, y), 0, Vector2(1, 4))
+		
+		
+func create_map():
+	pass
+	
+	
+func stop_walkers():
+	pass
+	
+	
+func reset_map():
+	pass
+	
+	
+func set_map_size(size: Vector2):
+	_map_size = size
+	
+	
+func get_map_size():
+	return _map_size
+	
+	
+func set_walker_count(count: int):
+	_walker_count = count
+	
+	
+func get_walker_count():
+	return _walker_count
+	
+	
+func set_walker_loops(loops: int):
+	_walker_loops = loops
+	
+	
+func get_walker_loops():
+	return _walker_loops
+	
+	
+func get_walker_birth_chance():
+	if _walkers.size() == 0:
+		return NAN
+		
+	return _walkers[0].get_birth_chance()
+	
+	
+func get_walker_death_chance():
+	if _walkers.size() == 0:
+		return NAN
+		
+	return _walkers[0].get_death_chance()
+	
+	
+func set_walker_birth_chance():
+	pass
+	
+	
+func set_walker_death_chance():
+	pass
+
+
+class Walker:
+	extends Node
+	
+	var _birth_chance: float
+	var _death_chance: float
+	
+	
+	func _init(birth_chance: float = 0.05, death_chance: float = 0.01):
+		_birth_chance = birth_chance
+		_death_chance = death_chance
+		
+		
+	func step():
+		pass
+		
+		
+	func should_birth():
+		pass
+		
+		
+	func should_die():
+		pass
+		
+		
+	func die():
+		self.queue_free()
+		
+		
+	func get_birth_chance():
+		return _birth_chance
+		
+		
+	func set_birth_chance(v: float):
+		_birth_chance = v
+		
+		
+	func get_death_chance():
+		return _death_chance
+		
+		
+	func set_death_chance(v: float):
+		_death_chance = v
--- scenes/ui/debug/map_generation.gd
+++ scenes/ui/debug/map_generation.gd
@@ -1,1 +1,100 @@
 extends CanvasLayer
+
+@onready var camera = %GameCamera as Camera2D
+@onready var map_manager = %MapManager
+@onready var count_spin_box = %CountSpinBox
+@onready var loops_spin_box = %LoopsSpinBox
+@onready var birth_spin_box = %BirthSpinBox
+@onready var death_spin_box = %DeathSpinBox
+@onready var x_spin_box = %XSpinBox
+@onready var y_spin_box = %YSpinBox
+@onready var submit_button = %SubmitButton
+@onready var clear_button = %ClearButton
+@onready var play_button = %PlayButton
+@onready var log_button = %LogButton
+@onready var zoom_in_button = %ZoomInButton as Button
+@onready var zoom_out_button = %ZoomOutButton as Button
+
+
+var counter: int = 1
+
+func _ready():
+	# signal connections
+	submit_button.pressed.connect(_on_submit_pressed)
+	clear_button.pressed.connect(_on_clear_pressed)
+	play_button.pressed.connect(_on_play_pressed)
+	log_button.pressed.connect(_on_log_pressed)
+	zoom_in_button.pressed.connect(_on_zoom_in_pressed)
+	zoom_out_button.pressed.connect(_on_zoom_out_pressed)
+
+	
+	# spin box defaults
+	count_spin_box.set_value_no_signal(map_manager.get_walker_count())
+	loops_spin_box.set_value_no_signal(map_manager.get_walker_loops())
+	birth_spin_box.set_value_no_signal(map_manager.get_walker_birth_chance())
+	death_spin_box.set_value_no_signal(map_manager.get_walker_death_chance())
+	x_spin_box.set_value_no_signal(map_manager.get_map_size().x)
+	y_spin_box.set_value_no_signal(map_manager.get_map_size().y)
+
+	# setup camera
+	camera.make_current()
+	camera.global_position = get_viewport().size / 2
+	camera.set_zoom(Vector2(0.15, 0.15))
+
+
+func _on_zoom_in_pressed():
+	camera.zoom *= 1.75
+
+
+func _on_zoom_out_pressed():
+	camera.zoom *= 0.75
+	
+	
+func _on_submit_pressed():
+	var count: int = count_spin_box.value
+	var loops: int = loops_spin_box.value
+	var birth_chance: float = birth_spin_box.value
+	var death_chance: float = death_spin_box.value
+	var map_size = Vector2(x_spin_box.value, y_spin_box.value)
+	
+	map_manager.set_map_size(map_size)
+	map_manager.set_walker_count(count)
+	map_manager.set_walker_loops(loops)
+	map_manager.set_walker_birth_chance(birth_chance)
+	map_manager.set_walker_death_chance(death_chance)
+	map_manager.reset_map()
+	
+	map_manager.create_map()
+	
+	
+func _on_clear_pressed():
+	count_spin_box.set_value_no_signal(10)
+	loops_spin_box.set_value_no_signal(100)
+	birth_spin_box.set_value_no_signal(0.05)
+	death_spin_box.set_value_no_signal(0.01)
+	x_spin_box.set_value_no_signal(500)
+	y_spin_box.set_value_no_signal(500)
+	
+	
+func _on_play_pressed():
+	var paused: bool = get_tree().paused
+	get_tree().paused = !paused
+	
+	if paused:
+		play_button.text = "Play"
+	else:
+		play_button.text = "Pause"
+	
+	
+func _on_log_pressed():
+	var count: int = count_spin_box.value
+	var loops: int = loops_spin_box.value
+	var birth_chance: float = birth_spin_box.value
+	var death_chance: float = death_spin_box.value
+	var map_size = Vector2(x_spin_box.value, y_spin_box.value)
+	
+	print_debug("Saved values #%d:\n\tWalker count: %d\n\tWalker loops: %d\n\t" + \
+		"Walker birth chance: %.3f\n\tWalker death chance: %.3f\n\t" + \
+		"Map size: %s\n" % [counter, count, loops, birth_chance, death_chance, map_size])
+	
+	counter += 1
